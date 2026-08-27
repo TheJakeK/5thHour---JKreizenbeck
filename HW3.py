4c569016e7f2b75c5ff3bbe89bb9f0c76dc89d69
Jake Kreizenbeck <jk129112@student.wpsok.org>
2026-08-27 12:43:41 -0500

    Submitted

---
+++ HW3.py
@@ -0,0 +1,47 @@
+#Name: Jake Kreizenbeck
+#Class: 5th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+
+print("Hello World")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+
+intVar1 = 4
+strVar2 = "Jake"
+boolVar3 = True
+
+#3. Print all three variables on the same print function (at the same time).
+
+print(intVar1 , strVar2 , boolVar3)
+
+#4. Create a variable that asks the user to input an integer.
+
+enter_integer = int(input("Enter an Integer"))
+
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+
+cruz = (enter_integer+intVar1)
+print(cruz)
+
+#6. Take the result from #5 and divide it by 2. Print the result.
+
+print(cruz/2)
+
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+
+boolVar3 = False
+
+#8. Print the value of the boolean variable.
+
+print(boolVar3)
+
+#9. Create a variable with a number that contains decimals.
+
+intrVar4 = 0.7
+
+#10. Round the number from #9 up or down using the round function.
+
+print(round(intrVar4))
