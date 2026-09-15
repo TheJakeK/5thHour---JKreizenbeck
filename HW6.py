65b7a3b7cc2397a8833ba91d6a3eb66bdbfa5073
Jake Kreizenbeck <jk129112@student.wpsok.org>
2026-09-15 12:56:33 -0500

    Submitted

--- HW6.py
+++ HW6.py
@@ -6,7 +6,7 @@
 #1. Create a list with 9 different numbers inside.
 num_list = [67, 89, 910, 2, 5, 32, 31, 8, 1]
 #2. Sort the list from highest to lowest.
-num_list.sort()
+num_list.sort(reverse=True)
 print(num_list)
 #3. Create an empty list.
 empt_list = []
